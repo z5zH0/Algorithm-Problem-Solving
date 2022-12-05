5366cf5b9ebf5f811e7f6dcbd735181b20f08ca2
yejin <90134500+z5zH0@users.noreply.github.com>
2022-12-05 12:22:15 -0800

    string

--- Programmers/programmers_words.py
+++ Programmers/programmers_words.py
@@ -1,3 +1,6 @@
+# link: https://school.programmers.co.kr/learn/courses/30/lessons/81301
+# solving method: string methods
+
 def solution(s):
     Neo = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     Prodo = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
